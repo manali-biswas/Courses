fa4b29ce5abd1c399868f63ae71f55ac82723ac4
Manali Biswas <46257679+manali-biswas@users.noreply.github.com>
2020-12-28 18:29:10 +0530

    add project form

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,39 @@
-import { Card, CardBody, CardText, CardTitle } from "reactstrap";
+import { Card, CardBody, CardText, CardTitle, Button, Modal, ModalHeader, ModalBody, Row, Col } from "reactstrap";
+import { LocalForm, Control, Errors } from "react-redux-form";
+import { Component } from "react";
 
-function Projects(props) {
+const required = (val) => val && val.length;
+const minLength = (len) => (val) => (val) && (val.length >= len);
+const maxLength = (len) => (val) => !val || (val.length <= len);
+
+class Projects extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isModalOpen: false
+        }
+
+        this.toggleModal = this.toggleModal.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        })
+    }
+
+    onSubmit(values) {
+        this.toggleModal();
+        alert("Current state is: " + JSON.stringify(values));
+    }
     
-        const RenderProject = props.projects.map((project) => {
+    
+    render() {
+            
+        const RenderProject = this.props.projects.map((project) => {
             return (  
                 <Card key={project.id} className="mb-3 border-dark">
                     <CardBody>
@@ -19,17 +50,56 @@ function Projects(props) {
         </Card>
             )
         }  
-    );
-    
-
+        );
+        
     return (
+        <>
         <div className="container mt-5 mb-5 text-center">
-            <h2>Projects on {props.course_name}</h2>
+            <h2>Projects on {this.props.course_name}</h2>
             <div className="mt-5 container w-75">
                 {RenderProject}
-                </div>
+            </div>
+            <Button color="outline-primary" onClick={this.toggleModal}>Add Project</Button>
         </div>
-    )
+
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+            <ModalHeader>Add Project</ModalHeader>
+                <ModalBody>
+                    <div className="container">
+                <LocalForm onSubmit={this.onSubmit}>
+                        <Row className="form-group">
+                            <Col className="label col-md-4" htmlFor="name">Project Name</Col>
+                                <Control.text model=".name" id="name" name="name" placeholder="Project Name" className="col-md-8 form-control" validators={{ required, minLength:minLength(2) }} />
+                                <Errors className="text-danger" model=".name" show="touched" messages={{
+                                    required: "Project name is required",
+                                    minLength: "Minimum 2 characters required"
+                                }}/>
+                    </Row>
+                        <Row className="form-group">
+                            <Col className="label col-md-4" htmlFor="description">Project Description</Col>
+                                <Control.textarea model=".description" id="description" name="description" rows="12" placeholder="Project Description" className="col-md-8 form-control" validators={{ required, maxLength: maxLength(30) }} />
+                                <Errors className="text-danger" model=".description" show="touched" messages={{
+                                    required: "Project description is required",
+                                    maxLength: "Maximum 30 characters allowed"
+                                }}/>
+                        </Row>
+                        <Row className="form-group">
+                            <Col className="label col-md-4" htmlFor="link">Project Link</Col>
+                                <Control.text type="url" model=".link" id="link" name="link" placeholder="Project link" className="col-md-8 form-control"/>
+                        </Row>
+                        <Row className="form-group">
+                            <Col className="label col-md-4" htmlFor="github">Github Link</Col>
+                                <Control.text type="url" model=".github" id="github" name="github" placeholder="Github link" className="col-md-8 form-control"/>
+                        </Row>
+                        <Row>
+                            <Button type="submit" color="primary" value="submit">Submit</Button>
+                        </Row>
+                        </LocalForm>
+                        </div>
+            </ModalBody>
+            </Modal>
+            </>
+    )}
 }
 
 export default Projects;
